45ee68bbace9e58d5a14400a71ded47e8e16f104
Tadek Teleżyński <tadekte@gmail.com>
2021-09-01 15:40:37 +0200

    Handle Dataview html elements in postporcessor

--- main.ts
+++ main.ts
@@ -396,8 +396,14 @@ export default class ReadwisePlugin extends Plugin {
       if (!ctx.sourcePath.startsWith(this.settings.readwiseDir)) {
         return;
       }
-      // @ts-ignore
-      let matches = [...ctx.getSectionInfo(el).text.matchAll(/__(.+)__/g)].map((a) => a[1]);
+      let matches: string[]
+      try {
+        // @ts-ignore
+        matches = [...ctx.getSectionInfo(el).text.matchAll(/__(.+)__/g)].map((a) => a[1]);
+      } catch (TypeError) {
+        // failed interaction with a Dataview element
+        return
+      }
       const hypers = el.findAll("strong").filter(e => matches.contains(e.textContent));
       hypers.forEach(strongEl => {
         const replacement = el.createEl('span');
